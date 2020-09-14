1016b806a64cea28892a4ad082bb861d5cda7f9e
musukha20 <musukhab2@gmail.com>
2020-09-14 12:31:05 +0530

    New Project

---
+++ NewGitProject/src/com/lti/HelloWorld.java
@@ -0,0 +1,23 @@
+package com.lti;
+
+
+	import java.time.LocalDateTime;
+	import java.util.Date;
+
+	public class HelloWorld {
+
+		public void main() {
+			System.out.println("Hello");
+			
+			Date d =new Date();
+			System.out.println(d);
+			LocalDateTime dt=LocalDateTime.now();
+			System.out.println(dt);
+			
+			System.out.println("Hello");
+			
+		}
+
+	}
+
+
